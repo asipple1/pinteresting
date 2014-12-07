d700e98d5e45a2d57983e35c97d09cea9d9c5ea3
Andrew Sipple <asipple1@gmail.com>
2014-12-07 16:43:03 -0600

    testing application.rb file

--- config/application.rb
+++ config/application.rb
@@ -23,9 +23,9 @@ module Pinteresting
     #Required for heroku
     config.assets.initialize_on_precompile = false
      # Add the fonts path
-    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    #config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
     # Precompile additional assets
-    config.assets.precompile += %w( .svg .eot .woff .ttf )
+    #config.assets.precompile += %w( .svg .eot .woff .ttf )
   end
 end
